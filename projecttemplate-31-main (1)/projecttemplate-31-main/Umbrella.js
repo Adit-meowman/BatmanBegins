a799200d77065c1b03e94db325394fa0e003fedf
Adit-meowman <75154717+Adit-meowman@users.noreply.github.com>
2021-05-15 12:48:35 +0530

    Add files via upload

---
+++ projecttemplate-31-main (1)/projecttemplate-31-main/Umbrella.js
@@ -0,0 +1,18 @@
+class Umbrella{
+    constructor(x,y){
+        var options = {
+            isStatic : true
+        }
+    this.image = loadImage("Walking Frame/walking_1.png")
+    this.umbrella = Bodies.circle(x,y,50,options);
+    this.radius = 50;
+    World.add(world,this.umbrella);
+
+    }
+
+    display(){
+     var position = this.umbrella.position;
+     imageMode(CENTER)
+     image(this.image,position.x,position.y+70,300,300)
+    }
+}
